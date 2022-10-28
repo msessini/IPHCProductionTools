8a7db10316f00c25d4e18c4ad1e29faa99b4f3c6
msessini <mario.sessini@etu.unistra.fr>
2022-10-28 09:33:56 +0200

    update

--- LLRHiggsTauTau/NtupleProducer/test/submitEmbedded_2018.sh
+++ LLRHiggsTauTau/NtupleProducer/test/submitEmbedded_2018.sh
@@ -15,6 +15,6 @@ sed -i 's/.*DataMCType.*/				       DataMCType    = cms.untracked.string("DY_mut
 crab submit config2018/embedded/crab3_EmbedC_2018.py
 
 sed -i 's/.*PERIOD =.*/PERIOD = '\''D'\''/g' analyzer.py
-sed -i 's/.*DataMCType.*/				       DataMCType    = #cms.untracked.string("DY_mutau_embedded")/g' ../python/HiggsTauTauProducer.py
+sed -i 's/.*DataMCType.*/				       DataMCType    = cms.untracked.string("DY_mutau_embedded")/g' ../python/HiggsTauTauProducer.py
 crab submit config2018/embedded/crab3_EmbedD_2018.py
 
